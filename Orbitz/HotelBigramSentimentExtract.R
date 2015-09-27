8e65170386fee5328abc02d9720cdae174f3a408
Parikshita Tripathi (Pari) <parikshita02@gmail.com>
2015-09-27 04:00:44 -0500

    Update HotelBigramSentimentExtract.R

--- Orbitz/HotelBigramSentimentExtract.R
+++ Orbitz/HotelBigramSentimentExtract.R
@@ -1,6 +1,8 @@
 # installing required libraries
 install.packages("stringr")
 library(stringr)
+library(tm)
+library(plyr)
 
 # read city_rating files 
 city_files <- list.files(pattern = "*_city_rating.csv")
@@ -8,14 +10,16 @@ city_files <- list.files(pattern = "*_city_rating.csv")
 for (i in 1:length(city_files))
 {
   # reading one city data at a time
-  city_df <- read.csv(city_files[1], header = TRUE)
+  city_df <- read.csv(city_files[21], header = TRUE)
+  
+  city_name <- as.character(unique(city_df$City))
   
   # create directory to save bigram table of each hotel of the city
   dir.create(file.path("C:/Users/Parikshita/Desktop/Data Science/Summer 2015/SummerPracticum/Orbitz"
-                       , unique(city_df$City)))
+                       , city_name, warning = FALSE))
   
   dirC <- file.path("C:/Users/Parikshita/Desktop/Data Science/Summer 2015/SummerPracticum/Orbitz"
-                    , unique(city_df$City))
+                    , city_name)
   
   #extracting unique names of the hotels within the city
   hotel_city <- as.character(unique(city_df$Hotel))
@@ -23,7 +27,7 @@ for (i in 1:length(city_files))
   for (j in 1:length(hotel_city))
   {
     hotel <- hotel_city[1]
-    hotel_filter <- which(sapply(lapply(1:nrow(city_rating), function(x) any(which(city_df$Hotel[x] %in% hotel)))
+    hotel_filter <- which(sapply(lapply(1:nrow(city_df), function(x) any(which(city_df$Hotel[x] %in% hotel)))
                                  , isTRUE))   
     
     hotel_rt <- city_df[hotel_filter,]
@@ -52,34 +56,59 @@ for (i in 1:length(city_files))
       uni_cb <- as.character(bigram_hash$bigram[x])
       
       # running loop for matching string with review rating 1
-      r1 <- count(sapply(lapply(1:nrow(city_df), function(y) 
-        (city_df$Rating[y] == 1 & str_detect(city_df$Content[y], uni_cb))), isTRUE))
+      #  r1 <- count(sapply(lapply(1:nrow(city_df), function(y) 
+      #    (city_df$Rating[y] == 1 & str_detect(city_df$Content[y], uni_cb))), isTRUE))
       
-      bigram_hash$rating_1[x] <- ifelse(length(r1$freq[r1$x == "TRUE"]) == 0, 0, r1$freq[r1$x == "TRUE"])
+      r1 <- count(sapply(lapply(1:nrow(city_df), function(y)
+        (city_df$Rating[y] == 1 & str_count(city_df$Content[y], uni_cb))), isTRUE))
+      
+      #bigram_hash$rating_1[x] <- r1
+      
+      bigram_hash$rating_1[x] <- as.numericifelse(ifelse(length(r1$freq[r1$x == "TRUE"]) == 0, 0, r1$freq[r1$x == "TRUE"]))
       
       # running loop for matching string with review rating 2
-      r2 <- count(sapply(lapply(1:nrow(city_df), function(y) 
-        (city_df$Rating[y] == 2 & str_detect(city_df$Content[y], uni_cb))), isTRUE))
+      #r2 <- count(sapply(lapply(1:nrow(city_df), function(y) 
+      #  (city_df$Rating[y] == 2 & str_detect(city_df$Content[y], uni_cb))), isTRUE))
       
-      bigram_hash$rating_2[x] <- ifelse(length(r2$freq[r2$x == "TRUE"]) == 0, 0, r2$freq[r2$x == "TRUE"])
+      r2 <- count(sapply(lapply(1:nrow(city_df), function(y)
+        (city_df$Rating[y] == 2 & str_count(city_df$Content[y], uni_cb))), isTRUE))
+      
+      #bigram_hash$rating_2[x] <- r2
+      
+      bigram_hash$rating_2[x] <- as.numericifelse(ifelse(length(r2$freq[r2$x == "TRUE"]) == 0, 0, r2$freq[r2$x == "TRUE"]))
       
       # running loop for matching string with review rating 3
-      r3 <- count(sapply(lapply(1:nrow(city_df), function(y) 
-        (city_df$Rating[y] == 3 & str_detect(city_df$Content[y], uni_cb))), isTRUE))
+      #r3 <- count(sapply(lapply(1:nrow(city_df), function(y) 
+      #  (city_df$Rating[y] == 3 & str_detect(city_df$Content[y], uni_cb))), isTRUE))
       
-      bigram_hash$rating_3[x] <- ifelse(length(r3$freq[r3$x == "TRUE"]) == 0, 0, r3$freq[r3$x == "TRUE"])
+      r3 <- count(sapply(lapply(1:nrow(city_df), function(y)
+        (city_df$Rating[y] == 3 & str_count(city_df$Content[y], uni_cb))), isTRUE))
+      
+      #bigram_hash$rating_3[x] <- r3
+      
+      bigram_hash$rating_3[x] <- as.numericifelse(ifelse(length(r3$freq[r3$x == "TRUE"]) == 0, 0, r3$freq[r3$x == "TRUE"]))
       
       # running loop for matching string with review rating 4
-      r4 <- count(sapply(lapply(1:nrow(city_df), function(y) 
-        (city_df$Rating[y] == 4 & str_detect(city_df$Content[y], uni_cb))), isTRUE))
+      #r4 <- count(sapply(lapply(1:nrow(city_df), function(y) 
+      #  (city_df$Rating[y] == 4 & str_detect(city_df$Content[y], uni_cb))), isTRUE))
       
-      bigram_hash$rating_4[x] <- ifelse(length(r4$freq[r4$x == "TRUE"]) == 0, 0, r4$freq[r4$x == "TRUE"])
+      r4 <- count(sapply(lapply(1:nrow(city_df), function(y)
+        (city_df$Rating[y] == 4 & str_count(city_df$Content[y], uni_cb))), isTRUE))
+      
+      #bigram_hash$rating_4[x] <- r4
+      
+      bigram_hash$rating_4[x] <- as.numericifelse(ifelse(length(r4$freq[r4$x == "TRUE"]) == 0, 0, r4$freq[r4$x == "TRUE"]))
       
       # running loop for matching string with review rating 5
-      r5 <- count(sapply(lapply(1:nrow(city_df), function(y) 
-        (city_df$Rating[y] == 5 & str_detect(city_df$Content[y], uni_cb))), isTRUE))
+      #r5 <- count(sapply(lapply(1:nrow(city_df), function(y) 
+      #  (city_df$Rating[y] == 5 & str_detect(city_df$Content[y], uni_cb))), isTRUE))
       
-      bigram_hash$rating_5[x] <- ifelse(length(r5$freq[r5$x == "TRUE"]) == 0, 0, r5$freq[r5$x == "TRUE"])
+      r5 <- count(sapply(lapply(1:nrow(city_df), function(y)
+        (city_df$Rating[y] == 5 & str_count(city_df$Content[y], uni_cb))), isTRUE))
+      
+      #bigram_hash$rating_5[x] <- r5
+        
+      bigram_hash$rating_5[x] <- as.numeric(ifelse(length(r5$freq[r5$x == "TRUE"]) == 0, 0, r5$freq[r5$x == "TRUE"]))
     }
     
     # removing bigrams with total count = 0
@@ -103,6 +132,6 @@ for (i in 1:length(city_files))
     
     # writing csv for each hotel
     bigram_hash <- data.frame(lapply(bigram_hash, as.character), stringsAsFactors=FALSE)
-    write.csv(bigram_hash, file.path(dirC, paste(hotel, "bigram_hash.csv", sep = "_")), row.names = FALSE)
+    write.csv(bigram_hash, file.path(dirC, paste("hotel1", "bigram_hash.csv", sep = "_")), row.names = FALSE)
   }
 }
